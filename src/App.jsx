2028185df0f1b343277c81fec0d6b123fa36ff3d
IbrahimDev007 <ibrahimpc4@gmail.com>
2023-04-06 22:42:35 +0600

    home page data added and lottie animation addded

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom";
+import About from "./components/About";
+import Heder from "./components/Heder";
+import Footer from "./components/Footer";
+
 function App() {
-  return (
-    <div className='App'>
-      <h1>Welcome to nextPage!</h1>
-    </div>
-  )
+	return (
+		<div>
+			<Heder />
+			<Outlet />
+			<Footer />
+		</div>
+	);
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
+import About from './components/About'
+import Books from './components/Books'
+import Home from './components/Home'
 import './index.css'
+const router=createBrowserRouter([
+    {
+    path:'/',
+    element:<App/>,
+    children:[
+        {
+        path:'/',
+        element:<Home/>,
+    },
+    {
+        path:'/about',
+        element:<About/>
+    },
+    {
+        path:'/books',
+        element:<Books/>
+    },
+]
+},
+    
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+<RouterProvider router={router}/>
+)
